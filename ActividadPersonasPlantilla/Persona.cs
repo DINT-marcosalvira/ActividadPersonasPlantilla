b5b149af5c42462a28685e78538295e5e3b009db
Marcos Alvira Romero <marcos.alvira@iesdoctorbalmis.com>
2019-11-13 14:11:54 +0100

    Agregar archivos de proyecto.

---
+++ ActividadPersonasPlantilla/Persona.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActividadPersonasPlantilla
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Foto { get; set; }
+        public string Sexo { get; set; }
+
+        public Persona(int id, string nombre, string foto, string sexo)
+        {
+            Id = id;
+            Nombre = nombre;
+            Foto = foto;
+            Sexo = sexo;
+        }
+
+        public static ObservableCollection<Persona> GetPersonas()
+        {
+            ObservableCollection<Persona> lista = new ObservableCollection<Persona>();
+
+            lista.Add(new Persona(1, "Pepe", "1.jpg", "Hombre"));
+            lista.Add(new Persona(2, "Antonio", "2.jpg", "Hombre"));
+            lista.Add(new Persona(3, "Sara", "3.jpg", "Mujer"));
+
+            return lista;
+        }
+    }
+}
---
+++ ActividadPersonasPlantilla/SexoConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace ActividadPersonasPlantilla
+{
+    class SexoConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string p = (string)value;
+            p = p.ToLower();
+            if (p.Equals("mujer"))
+            {
+                return "mujer.png";
+            }
+            else
+            {
+                return "hombre.png";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
